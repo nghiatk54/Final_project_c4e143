2d399e08a5b010a1a5dde633008297b681b520dd
nghiatk54 <nghiatk54@gmail.com>
2023-09-27 14:47:18 +0700

    Header

---
+++ assets/js/home.js
@@ -0,0 +1,15 @@
+const navBarIcon = document.querySelector(".navBar .navBar_icon");
+const navBarClose = document.querySelector(".navBar .navBar_close");
+const navBarGroup = document.querySelector(".navBar .navBar_group");
+
+navBarIcon.addEventListener("click", function () {
+    navBarClose.style.display = "inline-block";
+    navBarGroup.style.display = "flex";
+    navBarIcon.style.display = "none";
+});
+
+navBarClose.addEventListener("click", function () {
+    navBarClose.style.display = "none";
+    navBarGroup.style.display = "none";
+    navBarIcon.style.display = "inline-block";
+});
