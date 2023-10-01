b4a0d582e23d3f9831e9c8a01138d98ed336752f
nghiatk54 <nghiatk54@gmail.com>
2023-10-01 15:20:36 +0700

    Footer

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,3 @@
-let width = screen.width;
 const navBarIcon = document.querySelector(".navBar .navBar_icon");
 const navBarClose = document.querySelector(".navBar .navBar_close");
 const navBarGroup = document.querySelector(".navBar .navBar_group");
@@ -13,4 +12,17 @@ navBarClose.addEventListener("click", function () {
     navBarClose.style.display = "none";
     navBarGroup.style.display = "none";
     navBarIcon.style.display = "inline-block";
+});
+
+window.addEventListener("resize", () => {
+    let width = window.innerWidth;
+    if (width > 768) {
+        navBarClose.style.display = "none";
+        navBarIcon.style.display = "none";
+        navBarGroup.style.display = "flex";
+    } else {
+        navBarIcon.style.display = "inline-block";
+        navBarGroup.style.display = "none";
+        navBarClose.style.display = "none";
+    }
 });
